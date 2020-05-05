1ae26c9af42f82773d5599ad264c306fe43b4a2d
FedeClaudi <federicoclaudi@protonmail.com>
2020-05-05 11:46:13 +0100

    bump

--- 01_prep_data.py
+++ 01_prep_data.py
@@ -3,70 +3,21 @@
 import os
 import pandas as pd
 import numpy as np
-from oneibl.onelight import ONE
 import matplotlib.pyplot as plt
-from scipy.ndimage import gaussian_filter1d
-from fcutils.maths.utils import binArray
 from tqdm import tqdm 
 from scipy import stats
 import multiprocessing as mp
+from sklearn import decomposition, manifold
 
-
-# Get a session
-one = ONE()
-one.set_figshare_url = "https://figshare.com/articles/steinmetz/9974357"
-
-sessions =  one.search(['spikes'])
-
-
-
-sess_n = 36
-sess = sessions[36]
-
-# ----------------------------- Get metadata/data ---------------------------- #
-df_fp = os.path.join('data', f'sess{sess_n}_alldata.h5')
-if not os.path.isfile(df_fp):
-    # Download some data
-    channels_loc = one.load_dataset(sess, 'channels.brainLocation')
-
-    spikes_clust = one.load_dataset(sess, 'spikes.clusters')
-    spikes_times = (one.load_dataset(sess, 'spikes.times').ravel() * 1000).astype(np.int32) # convert to msecs
-
-    clust_probe = one.load_dataset(sess, 'clusters.probes')
-    clust_channel = one.load_dataset(sess, 'clusters.peakChannel')
-
-    print(f"Regions in session: {sorted(set(channels_loc.allen_ontology))}")
-
-    # Assign a spike to each cluster and each cluster to a brain region
-    spikes = pd.DataFrame(dict(cluster=spikes_clust.ravel(), times=spikes_times.ravel()))
-
-    spikes['channel']= [clust_channel[s[0]].ravel()[0] for s in spikes_clust]
-
-    channels_loc_dic = {i:row.allen_ontology for i,row in channels_loc.iterrows()}
-    spikes['region'] = [channels_loc_dic[int(ch-1)] for ch in spikes['channel']]
-
-    print(spikes)
-    print(set(spikes.region.values))
-
-    spikes.to_hdf(df_fp, key='hdf')
-else:
-    spikes = pd.read_hdf(df_fp)
-
-
-# Get spikes for one brain region at the time
-max_T = 10 * 60 * 1000 # 10 minutes
-time = np.zeros((max_T, 1))
-time_vals = np.arange(0, len(time))
-sigma = 100 # 100ms std
 
 def get_cell_frate(args):
-    time, cells, cell = args
+    time, cells, cell, sigma = args
     print(f'processing cell {cell} of {cells.shape[1]}')
 
     res = np.zeros_like(time).ravel()
     spike_times = np.where(cells[:, cell] == 1)[0]
 
-    for spike in spike_times:
+    for spike in tqdm(spike_times):
         gauss = stats.norm(spike, sigma)
         support = np.linspace(0, len(time), len(time))
         density = gauss.pdf(support)
@@ -74,64 +25,104 @@ def get_cell_frate(args):
         res = res + cells[:, cell] * density
     return res
 
-for region in ['SCm']:
-    # prep some file paths
-    frates_fp = os.path.join('data', f'sess_{sess_n}_{region}_frates.npy')
+def run():
+    from oneibl.onelight import ONE
+
+    # Get a session
+    one = ONE()
+    one.set_figshare_url = "https://figshare.com/articles/steinmetz/9974357"
+
+    sessions =  one.search(['spikes'])
 
 
-    reg = spikes.loc[spikes.region==region]
-    reg_clusters = list(set(reg.cluster.values))
-    print(f"Found {len(reg_clusters)} clusters for {region}")
 
-    # Create an array that is N_samples x N_cells and is 1 when a cell fires
-    cells = np.zeros((time.shape[0], len(reg_clusters)))
+    sess_n = 36
+    sess = sessions[36]
 
-    for n, clust in enumerate(reg_clusters):
-        clust_spikes = spikes.loc[(spikes.cluster == clust)&(spikes.times < max_T)]
-        cells[clust_spikes.times, n] = 1
-    cells = cells[:max_T, :]
+    # ----------------------------- Get metadata/data ---------------------------- #
+    df_fp = os.path.join('data', f'sess{sess_n}_alldata.h5')
+    if not os.path.isfile(df_fp):
+        # Download some data
+        channels_loc = one.load_dataset(sess, 'channels.brainLocation')
 
-    # Convert spikes to rates by summing with a gaussian kernel
-    if not os.path.isfile(frates_fp):
-        frates = np.zeros_like(cells)
+        spikes_clust = one.load_dataset(sess, 'spikes.clusters')
+        spikes_times = (one.load_dataset(sess, 'spikes.times').ravel() * 1000).astype(np.int32) # convert to msecs
 
-        pool = mp.Pool(mp.cpu_count()-2)
-        res = pool.map(get_cell_frate, [(time, cells, cell) for cell in np.arange(5)])
-        pool.close()
+        clust_probe = one.load_dataset(sess, 'clusters.probes')
+        clust_channel = one.load_dataset(sess, 'clusters.peakChannel')
 
-        np.save(frates_fp, frates)
+        print(f"Regions in session: {sorted(set(channels_loc.allen_ontology))}")
 
+        # Assign a spike to each cluster and each cluster to a brain region
+        spikes = pd.DataFrame(dict(cluster=spikes_clust.ravel(), times=spikes_times.ravel()))
 
+        spikes['channel']= [clust_channel[s[0]].ravel()[0] for s in spikes_clust]
+
+        channels_loc_dic = {i:row.allen_ontology for i,row in channels_loc.iterrows()}
+        spikes['region'] = [channels_loc_dic[int(ch-1)] for ch in spikes['channel']]
+
+        print(spikes)
+        print(set(spikes.region.values))
+
+        spikes.to_hdf(df_fp, key='hdf')
     else:
-        frates = np.load(frates_fp)
-
-        
+        spikes = pd.read_hdf(df_fp)
 
 
-        
-
-    # Bin spike counts in 1s bins for persistent homology
-
-    # embedd data into lower dimensional embeddiing spaces
-
-
+    # Get spikes for one brain region at the time
+    max_T = 10 * 60 * 1000 # 10 minutes
+    time = np.zeros((max_T, 1))
+    time_vals = np.arange(0, len(time))
+    sigma = 100 # 100ms std
 
 
+    for region in ['SCm']:
+        # prep some file paths
+        frates_fp = os.path.join('data', f'sess_{sess_n}_{region}_frates.npy')
+
+        reg = spikes.loc[spikes.region==region]
+        reg_clusters = list(set(reg.cluster.values))
+        print(f"Found {len(reg_clusters)} clusters for {region}")
+
+        # Create an array that is N_samples x N_cells and is 1 when a cell fires
+        print('Getting cells spikes')
+        cells = np.zeros((time.shape[0], len(reg_clusters)))
+
+        for n, clust in enumerate(reg_clusters):
+            clust_spikes = spikes.loc[(spikes.cluster == clust)&(spikes.times < max_T)]
+            cells[clust_spikes.times, n] = 1
+        cells = cells[:max_T, :]
+
+        # Convert spikes to rates by summing with a gaussian kernel
+        print('Getting firing rates')
+        if not os.path.isfile(frates_fp):
+            frates = np.zeros_like(cells)
+
+            pool = mp.Pool(mp.cpu_count()-2)
+            res = pool.map(get_cell_frate, [(time, cells, cell, sigma) for cell in np.arange(frates.shape[1])])
+            pool.close()
+
+            for n, rate in enumerate(res):
+                frates[:, n] = rate
+
+            np.save(frates_fp, frates)
+
+        else:
+            frates = np.load(frates_fp)
 
 
-# TODO smooth data
+        # TODO Bin spike counts in 1s bins for persistent homology
 
-# # bin data
-# binned = cells.reshape(-1, len(reg_clusters), 250)
-# binned = np.sum(binned, axis=-1)
+        # embedd data into lower dimensional embeddiing spaces to facilitate future analysi
+        print('Reducing dimensionality')
+        stable_frates = np.sqrt(frates)
 
-# plt.plot(binned[:, 0])
-
-# # save
-# np.save(f'{region}_cells.npy', cells)
-# np.save(f'{region}_cells_binned.npy', binned)
-# reg.to_hdf(f'{region}_spikes.h5', key='hdf5')
+        iso_instance = manifold.Isomap(5, 20) # 5 nearest neightbours and 20 dimensional embedding
+        proj_data = iso_instance.fit_transform(stable_frates)
+        np.save(os.path.join('data', 'SCm_iso_20.npy'), proj_data)
 
 
 
+if __name__ == "__main__":
+    run()
 
--- 02_dimred.py
+++ 02_dimred.py
@@ -7,42 +7,56 @@ import umap
 from vtkplotter import *
 from sklearn import decomposition, manifold
 from sklearn.decomposition import PCA
+import os
+
+from brainrender.scene import Scene
+
+from random  import choices
 
 # %%
 # ------------------------------ Params and load ----------------------------- #
 load_isomap = False
-load_umap = True
+load_umap = False
+N_SAMPLES =  100000
 
-
-regions = ['RSP', 'SCm']
-regdata = namedtuple('regdata', 'spikes cells binned')
+regions = ['SCm']
+regdata = namedtuple('regdata', 'spikes rates')
 data = {r:regdata(
-            pd.read_hdf(f'{r}_spikes.h5'), np.load(f'{r}_cells.npy'), np.load(f'{r}_cells_binned.npy')
-                ) for r in regions }
+            pd.read_hdf(os.path.join('data', 'sess36_alldata.h5')), 
+                    np.load(os.path.join('data', f'sess_36_{r}_frates.npy'))) for r in regions }
+
+random_idxs = choices(np.arange(data['SCm'].rates.shape[0]), k=N_SAMPLES)
 
 # %%
 # ------------------------------------ PCA ----------------------------------- #
 print('PCA')
-pca = PCA(n_components=3).fit_transform(data['SCm'].binned)
+
+pca = PCA(n_components=3).fit_transform(data['SCm'].rates[random_idxs, :])
 
 coords = [pca[i, :] for i in np.arange(pca.shape[0])]
 pca_points = Spheres(coords, r=.25, c='green', alpha=.8)
-
+# show(pca_points)
 # %%
 # ---------------------------------- Isomap ---------------------------------- #
 print('Isomap')
 if load_isomap:
     proj_data = np.load('SCm_iso_3.npy')
 else:
-    iso_instance = manifold.Isomap(15, 3)
-    proj_data = iso_instance.fit_transform(data['SCm'].binned)
+    iso_instance = manifold.Isomap(4, 3)
+
+    proj_data = iso_instance.fit_transform(data['SCm'].rates[random_idxs, :])
     np.save('SCm_iso_3.npy', proj_data)
 
 
-coords = [proj_data[i, :] for i in np.arange(proj_data.shape[0])]
-iso_points = Spheres(coords, r=.25, c='salmon', alpha=.8)
+# coords = [proj_data[i, :] for i in np.arange(proj_data.shape[0])]
+# iso_points = Spheres(coords, r=.005, c='salmon', alpha=.8)
 
+coords = pd.DataFrame(dict(x=proj_data[:, 0], y=proj_data[:, 1], z=proj_data[:, 2]))
 
+isoscene = Scene(add_root=False, display_inset=False, title='isomap')
+isoscene.add_cells(coords, radius=0.0025, color='salmon', res=24)
+isoscene.render()
+# show(iso_points, newPlotter=True)
 
 
 # %%
@@ -56,17 +70,19 @@ _umap_params = dict(
 
 if not load_umap:
     umapper = umap.UMAP(**_umap_params)
-    umapped = umapper.fit_transform(data['SCm'].binned)
+
+    umapped = umapper.fit_transform(data['SCm'].rates[random_idxs, :])
     np.save('SCm_umap_3.npy', umapped)
 else:
     umapped = np.load('SCm_umap_3.npy')
 
-coords = [umapped[i, :] for i in np.arange(umapped.shape[0])]
-umap_points = Spheres(coords, r=.25, c='blue')
+# coords = [umapped[i, :] for i in np.arange(umapped.shape[0])]
+# umap_points = Spheres(coords, r=.25, c='blue')
 
 
+coords = pd.DataFrame(dict(x=umapped[:, 0], y=umapped[:, 1], z=umapped[:, 2]))
 
-# %%
-# -------------------------------- Show render ------------------------------- #
-show(pca_points, iso_points, umap_points, shape=[1, 3])
+isoscene = Scene(add_root=False, display_inset=False, title='umap')
+isoscene.add_cells(coords, radius=.5, color='skyeblue', res=24)
+isoscene.render()
 
